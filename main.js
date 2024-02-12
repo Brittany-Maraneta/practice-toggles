1d644f9f87769e252351be1ab9def6209fe49723
Brittany-Maraneta <Brittany-june@hotmail.com>
2024-02-12 16:58:26 -0700

    push

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-let darkToggle = document.querySelector("#darkToggle");
+// the toggle button to control hide/show
+const toggleMobileMenu = document.querySelector("#menu-button");
 
-darkToggle.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+// the nav items to hide and show
+const gridContainer = document.querySelector("#grid-container");
+
+// the site header to click off the nav and close it
+const siteHeader = document.querySelector("#site-header");
+
+// the mobile media query to passivly close the menu when user goes to desktop size
+const mobileMediaQuery = window.matchMedia("(max-width: 550px)");
+
+// remove the mobile menu from the nav bar
+// mobile menu event listener toggle
+toggleMobileMenu.addEventListener("click", () => {
+  //when user clicks, show the menu with classList.toggle
+  gridContainer.classList.toggle("menu-row");
 });
+
+// automatically collapse the mobile menu when switch to desktop
+//add an event listener to the media query
+mobileMediaQuery.addEventListener("change", () => {
+  gridContainer.classList.remove("menu-row");
+});
+
+// add an event listener to the whole document
+document.addEventListener("click", (e) => {
+  if (!(e.target === siteHeader || siteHeader.contains(e.target))) {
+    gridContainer.classList.remove("menu-row");
+  }
+});
+
+// if the user clicks anywhere except for the site header and its children
+// remove the menu-row
